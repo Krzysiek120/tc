7f5fc813e703abba782c813409552e86819b9aee
Krzysztof Kmieć <9krzysiek2@gmail.com>
2018-11-03 18:18:16 +0100

    Dodanie formularza do helpdeska

--- src/Controller/HelpdeskController.php
+++ src/Controller/HelpdeskController.php
@@ -8,6 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
  * @Security("has_role('ROLE_USER')")
@@ -16,6 +19,17 @@ class HelpdeskController extends Controller
 {
     public function index(Request $request)
     {
-        return $this->render('helpdesk/index.html.twig', []);
+
+
+        $form = $this->createFormBuilder()
+            ->add('Title', TextType::class, array('label' => 'Tytuł'))
+            ->add('content', TextType::class, array('label' => 'Opis sprawy'))
+            ->add('attachment', FileType::class, array('label' => 'Dodaj załącznik'))
+            ->add('save', SubmitType::class, array('label' => 'Wyślij'))
+            ->getForm();
+
+        return $this->render('helpdesk/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
